b4687379ceeb11c52361aefbc58b39bf0e9d97a7
Phoenix <fphoenix923@hotmail.com>
2016-07-03 17:47:33 +0800

    建立Homework專案，撰寫測試情境

---
+++ PagingGroup/PagingGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagingGroup
+{
+	public class PagingGroup
+	{
+		public IEnumerable<int> GroupingFieldValues<T>(IEnumerable<T> data, string fieldName, int pagingCount)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
