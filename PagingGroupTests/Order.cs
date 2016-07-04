bfbce6c8095c3b6c4ad9b73687e41423ae40fbbc
Phoenix <fphoenix923@hotmail.com>
2016-07-04 10:50:55 +0800

    將測試資料改成Order

---
+++ PagingGroupTests/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace PagingGroup.Tests
+{
+	internal class Order
+	{
+		public int Id { get; set; }
+		public int Cost { get; set; }
+		public int Revence { get; set; }
+		public int SellPrice { get; set; }
+	}
+}
--- PagingGroupTests/PagingGroupTests.cs
+++ PagingGroupTests/PagingGroupTests.cs
@@ -9,114 +9,114 @@ using ExpectedObjects;
 
 namespace PagingGroup.Tests
 {
-    [TestClass()]
-    public class PagingGroupTests
-    {
-        private IEnumerable<dynamic> GetTestData() => new[]{
-            new {
-                Id=1,
-                Cost = 1,
-                Revence = 11,
-                SellPrice = 21
-            },
-            new {
-                Id=2,
-                Cost = 2,
-                Revence = 12,
-                SellPrice = 22
-            },
-            new {
-                Id=3,
-                Cost = 3,
-                Revence = 13,
-                SellPrice = 23
-            },
-            new {
-                Id=4,
-                Cost = 4,
-                Revence = 14,
-                SellPrice = 24
-            },
-            new {
-                Id=5,
-                Cost = 5,
-                Revence = 15,
-                SellPrice = 25
-            },
-            new {
-                Id=6,
-                Cost = 6,
-                Revence = 16,
-                SellPrice = 26
-            },
-            new {
-                Id=7,
-                Cost = 7,
-                Revence = 17,
-                SellPrice = 27
-            },
-            new {
-                Id=8,
-                Cost = 8,
-                Revence = 18,
-                SellPrice = 28
-            },
-            new {
-                Id=9,
-                Cost = 9,
-                Revence = 19,
-                SellPrice = 29
-            },
-            new {
-                Id=10,
-                Cost = 10,
-                Revence = 20,
-                SellPrice = 30
-            },
-            new {
-                Id=11,
-                Cost = 11,
-                Revence = 21,
-                SellPrice = 31
-            }
-        };
+	[TestClass()]
+	public class PagingGroupTests
+	{
+		private IEnumerable<Order> GetOrders() => new[]{
+			new Order{
+				Id=1,
+				Cost = 1,
+				Revence = 11,
+				SellPrice = 21
+			},
+			new Order{
+				Id=2,
+				Cost = 2,
+				Revence = 12,
+				SellPrice = 22
+			},
+			new Order {
+				Id=3,
+				Cost = 3,
+				Revence = 13,
+				SellPrice = 23
+			},
+			new Order{
+				Id=4,
+				Cost = 4,
+				Revence = 14,
+				SellPrice = 24
+			},
+			new Order{
+				Id=5,
+				Cost = 5,
+				Revence = 15,
+				SellPrice = 25
+			},
+			new Order{
+				Id=6,
+				Cost = 6,
+				Revence = 16,
+				SellPrice = 26
+			},
+			new Order{
+				Id=7,
+				Cost = 7,
+				Revence = 17,
+				SellPrice = 27
+			},
+			new Order{
+				Id=8,
+				Cost = 8,
+				Revence = 18,
+				SellPrice = 28
+			},
+			new Order{
+				Id=9,
+				Cost = 9,
+				Revence = 19,
+				SellPrice = 29
+			},
+			new Order{
+				Id=10,
+				Cost = 10,
+				Revence = 20,
+				SellPrice = 30
+			},
+			new Order{
+				Id=11,
+				Cost = 11,
+				Revence = 21,
+				SellPrice = 31
+			}
+		};
 
-        [TestMethod()]
-        public void GroupingFieldValuesTest_輸入欄位名稱Cost_3筆一組會得到結果6_15_24_21()
-        {
-            // arrange
-            var sut = new PagingGroup();
+		[TestMethod()]
+		public void GroupingFieldValuesTest_輸入欄位名稱Cost_3筆一組會得到結果6_15_24_21()
+		{
+			// arrange
+			var sut = new PagingGroup();
 
-            var testData = GetTestData();
-            var fieldName = "Cost";
-            var pagingCount = 3;
+			var testData = GetOrders();
+			var fieldName = "Cost";
+			var pagingCount = 3;
 
-            // act
-            var actual = sut.GroupingFieldValues(testData, fieldName, pagingCount);
+			// act
+			var actual = sut.GroupingFieldValues(testData, fieldName, pagingCount);
 
-            // assert
-            var expected = new[] { 6, 15, 24, 21 };
+			// assert
+			var expected = new[] { 6, 15, 24, 21 };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
-        }
+			expected.ToExpectedObject().ShouldEqual(actual);
+		}
 
-        [TestMethod()]
-        public void GroupingFieldValuesTest_輸入欄位名稱Revenue_4筆一組會得到結果50_66_60()
-        {
-            // arrange
-            var sut = new PagingGroup();
+		[TestMethod()]
+		public void GroupingFieldValuesTest_輸入欄位名稱Revenue_4筆一組會得到結果50_66_60()
+		{
+			// arrange
+			var sut = new PagingGroup();
 
-            var testData = GetTestData();
-            var fieldName = "Revence";
-            var pagingCount = 4;
+			var testData = GetOrders();
+			var fieldName = "Revence";
+			var pagingCount = 4;
 
-            // act
-            var actual = sut.GroupingFieldValues(testData, fieldName, pagingCount);
+			// act
+			var actual = sut.GroupingFieldValues(testData, fieldName, pagingCount);
 
-            // assert
-            var expected = new[] { 50, 66, 60 };
+			// assert
+			var expected = new[] { 50, 66, 60 };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
-        }
-    }
+			expected.ToExpectedObject().ShouldEqual(actual);
+		}
+	}
 }
